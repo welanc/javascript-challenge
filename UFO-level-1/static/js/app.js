3c4d10a99ab9c90abfd30c9200930066c8656c44
unknown <welan.c.13+gh@gmail.com>
2021-03-12 23:58:47 +1100

    completed UFO-Level-1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,57 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Select the tbody to populate with data later
+var tbody = d3.select("tbody");
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Clear all data from previous searches
+    tbody.html("");
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element and get the raw HTML node
+    var userInput = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var userValue = userInput.property("value");
+
+    console.log(userValue);
+    console.log(tableData);
+
+    // Filter data to user inputted value and store as variable
+    var filteredData = tableData.filter(date => date.datetime === userValue);
+
+    console.log(filteredData);
+
+    filteredData.forEach((ufoSighting) => {
+        // Use d3 to append one table row `tr` for each UFO sighting object
+        var row = tbody.append("tr");
+
+        // Use `Object.entries` to console.log each UFO sighting alue
+        var keyValuePairs = Object.entries(ufoSighting)
+
+        keyValuePairs.forEach(([key, value]) => {
+            console.log(`${key} -> ${value}`);
+
+            // Use d3 to append 1 cell per UFO sighting value 
+            // (Date, City, State, Country, Shape, Duration, Comments)
+            var cell = row.append("td")
+            cell.text(value);
+        });
+    });
+};
