4e1ff30d774ac6c93f63df1f35055888303ff4ae
unknown <welan.c.13+gh@gmail.com>
2021-03-14 01:55:18 +1100

    UFO-Level-2 completed

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,8 +17,10 @@ var filters = d3.select("#filters");
 
 // Select the "ul" by choosing the 
 var selectDate = d3.select("#select-date")
-var selectCountry = d3.select("#select-country");
+var selectCity = d3.select("#select-city");
 var selectState = d3.select("#select-state");
+var selectCountry = d3.select("#select-country");
+var selectShape = d3.select("#select-shape");
 
 // Get the dates from the data
 var dateMapped = tableData.map(sightings => sightings.datetime);
@@ -33,14 +35,148 @@ uniqueDates.forEach((date) => {
 });
 
 
+
 // Filtered map based on previous search criteria
-function mapCriteria(inputKey, userValue, criteria) {
+function mapCriteria(criteria, inputKey, userValue) {
     // Get the dates from the data
     var criteriaMapped = tableData.filter(sightings => sightings[inputKey] === userValue);
     var crtieriaFiltered = criteriaMapped.map(sightings => sightings[criteria]);
     // Get only unique dates from data
     var uniqueCriteria = [...new Set(crtieriaFiltered)];
     return uniqueCriteria;
+};
+
+// Create event handlers 
+selectDate.on("change", firstEnter);
+
+// Complete the event handler function for the form
+function firstEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear list if it is not empty
+    selectCity.html("");
+    var getDate = d3.select("#select-date").property("value");
+    var uniqueCities = mapCriteria("city", "datetime", getDate);
+
+    console.log(uniqueCities);
+
+    var cityPlaceHolder = selectCity.append("option").text("Select City");
+    uniqueCities.forEach((city) => {
+        selectCity.append("option").text(city);
+    });
+};
+
+// Create event handlers 
+selectCity.on("change", secondEnter);
+
+
+// Complete the event handler function for the form
+function secondEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear list if it is not empty
+    selectState.html("");
+
+    var getCity = d3.select("#select-city").property("value");
+    var uniqueStates = mapCriteria("state", "city", getCity);
+
+    console.log(uniqueStates);
+
+    var statePlaceHolder = selectState.append("option").text("Select State");
+    uniqueStates.forEach((state) => {
+        selectState.append("option").text(state);
+    });
+};
+
+// Create event handlers 
+selectState.on("change", thirdEnter);
+
+
+// Complete the event handler function for the form
+function thirdEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear list if it is not empty
+    selectCountry.html("");
+    var getState = d3.select("#select-state").property("value");
+    var uniqueCountries = mapCriteria("country", "state", getState);
+
+    console.log(uniqueCountries);
+
+    var countryPlaceHolder = selectCountry.append("option").text("Select Country");
+    uniqueCountries.forEach((country) => {
+        selectCountry.append("option").text(country);
+    });
+};
+
+// Create event handlers 
+selectCountry.on("change", fourthEnter);
+
+// Complete the event handler function for the form
+function fourthEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Clear list if it is not empty
+    selectShape.html("");
+    var getCity = d3.select("#select-city").property("value");
+    var uniqueShapes = mapCriteria("shape", "city", getCity);
+
+    console.log(uniqueShapes);
+
+    var shapePlaceHolder = selectShape.append("option").text("Select Shape");
+    uniqueShapes.forEach((shape) => {
+        selectShape.append("option").text(shape);
+    });
+};
+
+
+
+selectShape.on("change", finalEnter);
+button.on("click", finalEnter);
+
+
+function finalEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    dateValue = selectDate.property("value");
+    cityValue = selectCity.property("value");
+    stateValue = selectState.property("value");
+    countryValue = selectCountry.property("value");
+    shapeValue = selectShape.property("value");
+
+    var filteredData = tableData.filter(sighting =>
+        (sighting.datetime === dateValue) &&
+        (sighting.city === cityValue) &&
+        (sighting.state === stateValue) &&
+        (sighting.country === countryValue) &&
+        (sighting.shape === shapeValue)
+    );
+
+    filteredData.forEach((ufoSighting) => {
+        // Use d3 to append one table row `tr` for each UFO sighting object
+        var row = tbody.append("tr");
+
+        // Use `Object.entries` to console.log each UFO sighting alue
+        var keyValuePairs = Object.entries(ufoSighting)
+
+        keyValuePairs.forEach(([key, value]) => {
+            console.log(`${key} -> ${value}`);
+
+            // Use d3 to append 1 cell per UFO sighting value 
+            // (Date, City, State, Country, Shape, Duration, Comments)
+            var cell = row.append("td")
+            cell.text(value);
+        });
+    });
 };
 
 // // Function to create new list to hold select dropdown
@@ -53,52 +189,7 @@ function mapCriteria(inputKey, userValue, criteria) {
 //     return newSelect;
 // };
 
-// Create event handlers 
-selectDate.on("change", firstEnter);
 
-// Complete the event handler function for the form
-function firstEnter() {
-
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-
-    // Clear list if it is not empty
-    selectCountry.html("");
-
-    var getDate = d3.select("#select-date").property("value");
-    var uniqueCountries = mapCriteria("datetime", getDate, "country");
-
-    console.log(uniqueCountries);
-
-    var countryPlaceHolder = selectCountry.append("option").text("Select Country");
-    uniqueCountries.forEach((country) => {
-        selectCountry.append("option").text(country);
-    });
-};
-
-// Create event handlers 
-selectCountry.on("change", secondEnter);
-
-
-// Complete the event handler function for the form
-function secondEnter() {
-
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
-
-    // Clear list if it is not empty
-    selectState.html("");
-
-    var getCountry = d3.select("#select-country").property("value");
-    var uniqueStates = mapCriteria("country", getCountry, "state");
-
-    console.log(uniqueStates);
-
-    var countryPlaceHolder = selectState.append("option").text("Select Country");
-    uniqueStates.forEach((state) => {
-        selectState.append("option").text(state);
-    });
-};
 
 // // Select the input element and get the raw HTML node
 // var userInput = d3.select("#datetime");
